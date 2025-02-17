3b3b2711143ec570b6b0b23feec2c7eef61d78bf
jwwasylenko <jwwasy@gmail.com>
2025-02-17 16:44:35 -0700

    Add files via upload

---
+++ triptomontreal.js
@@ -0,0 +1,44 @@
+august29th.addEventListener('click',function(){
+    console.log("Day being changed");
+    document.getElementById("banner").src="images/Mountroyal000.jpg";
+    document.getElementById("date").innerHTML="August 29th 2024";
+    console.log("photo being changed");
+    document.getElementById("anecdotes").innerHTML="The flight left just after midnight and we arived in Montreal at 6AM. Walked up Mount Royal.";
+    console.log("anecdotes changing");
+})
+
+august30th.addEventListener('click',function(){
+    console.log("Day being changed");
+    document.getElementById("banner").src="images/Parclafontaine000.jpg";
+    document.getElementById("date").innerHTML="August 30th 2024";
+    console.log("photo being changed");
+    document.getElementById("anecdotes").innerHTML="On Friday I was looking for a shower. Sat on a bench in Parc Lafontaine";
+    console.log("anecdotes changing");
+
+})
+
+august31st.addEventListener('click',function(){
+    console.log("Day being changed");
+    document.getElementById("banner").src="images/Billards000.jpg";
+    document.getElementById("date").innerHTML="August 31st 2024";
+    console.log("photo being changed");
+    document.getElementById("anecdotes").innerHTML="This was Saturday night and this photo was taken at Apartment 200 where we arrived far too early.";
+
+})
+
+september1st.addEventListener('click',function(){
+    console.log("Day being changed");
+    document.getElementById("banner").src="images/Picnic_1000.jpg";
+    document.getElementById("date").innerHTML="September 1st 2024";
+    console.log("photo being changed");
+    document.getElementById("anecdotes").innerHTML="Sunday.";
+
+})
+
+september2nd.addEventListener('click',function(){
+    console.log("Day being changed");
+    document.getElementById("banner").src="images/Parclafontaine2000.jpg";
+    document.getElementById("date").innerHTML="September 2nd 2024";
+    console.log("photo being changed");
+    document.getElementById("anecdotes").innerHTML="Monday. Labour day.";
+})
